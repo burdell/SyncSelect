6246a0afc7baf487ec65ba8491f3850675be8d26
Nathan Griffin <nathangriffin@Nathans-MacBook-Pro-2.local>
2012-11-17 10:45:03 -0500

    js stuff

--- syncselect.js
+++ syncselect.js
@@ -1,5 +1,17 @@
 (function ( $ ) {
-	$.fn.syncSelect = function() {
+	var methods = {
+		init: function(){
+			var foo = "bar";
+		}
+	}
 
+	$.fn.syncSelect = function( method ){
+		if ( methods[method] ) {
+	  		return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+	  		return methods.init.apply( this, arguments );
+		} else {
+	  		$.error( 'Method ' +  method + ' does not exist on jQuery.syncSelect' );
+		}
 	};
 })( jQuery )
