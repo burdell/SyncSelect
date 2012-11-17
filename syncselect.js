080628dd9b687b8d2e5dbda2349bd7ba16cc6d9f
Nathan Griffin <nathanjgriffin@gmail.com>
2012-11-17 11:45:30 -0500

    method to create source, method to remove options

--- syncselect.js
+++ syncselect.js
@@ -1,7 +1,32 @@
 (function ( $ ) {
+	function createSource(selectOptions){
+		var source = {};
+		$.each(selectOptions, function(i, option){
+			var $option = $(option);
+			if ($option.val() != ""){
+				source[$option.val()] = $option.text(); 
+			}
+		});
+		return source;
+	}
+
 	var methods = {
 		init: function(){
-			var foo = "bar";
+			var selectOptions = this.eq(0).find('option');
+			this.source = createSource(this.selectOptions);
+			this.previousSelectedValue = null;
+			console.log(this);
+		},
+		setSelected: function(value){
+			$.each(this, function(i, selectList){
+				var $options = $(selectList).find('option');
+				$.each($options, function(i, option){
+					var $option = $(option);
+					if ($option.val() == value || $option.text() == value){
+						$option.remove();
+					}
+				}); 
+			});
 		}
 	}
 
